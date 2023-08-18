52fa12dd726c1ae3b9e45039a45fd3165a6e079a
yeomsungpil <present7256@gmail.com>
2023-08-18 15:07:48 +0900

    URLComponents 리팩토링 완료

--- TMDBTVSeasonExample/WitcherManager.swift
+++ TMDBTVSeasonExample/WitcherManager.swift
@@ -12,38 +12,31 @@ class WitcherManager {
     static let shared = WitcherManager()
     private init() {}
     
-
-    
     func callRequest(season: Int, episode: Int, completionHandler: @escaping(WitcherEpisode) -> Void) {
        
-        let url = "https://api.themoviedb.org/3/tv/71912/season/\(season)/episode/\(episode)?api_key=\(APIKey.tmdbKey)&language=ko"
-        AF.request(url).validate()
-            .responseDecodable(of: WitcherEpisode.self) { response in
-                guard let response = response.value else { return }
-                completionHandler(response)
-            }
-    }
-    
-    func callTwoRequest(episode: Int, completionHandler: @escaping(WitcherEpisode) -> Void) {
-       
-        let url = "https://api.themoviedb.org/3/tv/71912/season/2/episode/\(episode)?api_key=\(APIKey.tmdbKey)&language=ko"
+//        let url = "https://api.themoviedb.org/3/tv/71912/season/\(season)/episode/\(episode)?api_key=\(APIKey.tmdbKey)&language=ko"
+        
+        var components = URLComponents(string: "https://api.themoviedb.org/3/tv/71912/season/\(season)/episode/\(episode)")!
+        let apiKey = URLQueryItem(name: "api_key", value: "\(APIKey.tmdbKey)")
+        let language = URLQueryItem(name: "language", value: "ko")
+        components.queryItems = [apiKey, language]
+        let url = components.url!
+        
+        
+        let urlaa = "https://api.themoviedb.org/3/tv/71912/season/\(season)/episode/\(episode)?api_key=\(APIKey.tmdbKey)&language=ko"
+        let componentsaa = URLComponents(string: urlaa)
+
+        componentsaa?.scheme // https
+        componentsaa?.host   // api.themoviedb.org
+        componentsaa?.path   // 3/tv/71912/season/\(season)/episode/\(episode)
+        componentsaa?.query  // api_key=\(APIKey.tmdbKey)&language=ko
         
         AF.request(url).validate()
             .responseDecodable(of: WitcherEpisode.self) { response in
                 guard let response = response.value else { return }
                 completionHandler(response)
             }
-    }
-    
-    func callThreeRequest(episode: Int, completionHandler: @escaping(WitcherEpisode) -> Void) {
-       
-        let url = "https://api.themoviedb.org/3/tv/71912/season/3/episode/\(episode)?api_key=\(APIKey.tmdbKey)&language=ko"
         
-        AF.request(url).validate()
-            .responseDecodable(of: WitcherEpisode.self) { response in
-                guard let response = response.value else { return }
-                completionHandler(response)
-            }
+ 
     }
-    
 }
